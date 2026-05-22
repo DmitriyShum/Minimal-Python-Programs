900a95e27d1c253fcf975026f756182118dcaccd
Dmitriy. S <contact@dhotspot.dev>
2026-05-22 10:46:19 -0400

    Update GuessTheCombination.py
    Minimized code, but now it asks user if they want a hint every wrong guess.

--- GuessTheCombination.py
+++ GuessTheCombination.py
@@ -1,6 +1,6 @@
 # Author: Dmitriy.S - Dhotspot.dev
-# Update: 08/04/25
-# Ver   : Release 1.1
+# Update: 05/22/26
+# Ver   : Release 1.2
 # About : This is a combination guess game, where the user guesses a four digit combination. 
 # If it's wrong, they get a hint, and until the combination is correct, the user can keep guessing. I did not include running_time for this program, so people don't get confused with the numbers.
 
@@ -9,35 +9,24 @@ and may be used by anyone as desired, as well as sold. If there are any issues o
 open a new issue on this repo, and include the information."""
 
 import random as rn
+def CombinationGuess():
+    random_combo = rn.randrange(0000, 9999)
 
-def guess():
-    game = True
-    combset = range(1111, 9999)
-    random_combo = rn.choice(combset)
-    print(random_combo)
-    while game:
-        guess = int(input("Input your combination guess: ")) # The first guess, before the hint.
+    while True:
+        guess = int(input("Input your combination guess: "))
         if guess > 9999 or guess < 0000: # Error handling
             print("Number is out of range. Guess between 0000 and 9999.")
-        if guess > random_combo - 100 or guess < random_combo - 100:
-             print("Getting warmer!")
         if guess == random_combo: # If combo is correct
-            print("Correct combination")
-            quit()
+            print("Correct combination!")
+            break
+        if abs(guess - random_combo) == 20:
+             print("Guess combination is 20 numbers off!")
         else: # If combo is wrong.
             hint = input("Combination is wrong. Would you like a hint? (y/n)")
             if hint == "y":
-                print("The first two numbers of the combination is %.2s" % int(random_combo/100))
-            if hint == "n":
-                guess = int(input("Input your combination guess:")) # Second round. There's only two rounds which keep repeating.
-                if guess > 9999 or guess < 0000:
-                    print("Number is out of range. Guess between 0000 and 9999.")
-        if guess == random_combo:
-            print("Correct combination")
-            quit()
-        else:
-            print("Combination is wrong.")
-                    
+                print("The first two numbers of the combination are %.2s" % int(random_combo/100))
+            else:
+                continue
 
 if __name__ == "__main__":
-            guess()
+     CombinationGuess()
